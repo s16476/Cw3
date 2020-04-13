de670bab9fad83088871efbe74c0bcf7840f5621
Daniel Pawłowski <eldan3k@gmail.com>
2020-04-13 21:23:14 +0200

    init

---
+++ api/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Kowalski, Majewski, Andrzejewski";
+        }
+    }
+}
