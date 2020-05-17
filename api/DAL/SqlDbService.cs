4c4be79c4adf74ca152592a4752e22a22cd49f64
Daniel Pawłowski <eldan3k@gmail.com>
2020-05-17 22:12:24 +0200

    cw5.1

--- api/DAL/SqlDbService.cs
+++ api/DAL/SqlDbService.cs
@@ -72,22 +72,82 @@ namespace api.DAL
             var command = new SqlCommand();
             command.Connection = connection;
             var tran = connection.BeginTransaction();
-
-
             
-            command.CommandText = "select count(s.IdStudy) from studies s where name = @studiesTitle";
+
+            //Czy istnieje kierunek
+            command.CommandText = "select s from studies s where name = @studiesTitle";
             command.Parameters.AddWithValue("studiesTitle", enrollments.Studies);
             connection.Open();
             var data = command.ExecuteReader();
-            var enrollment = 0;
-            while (data.Read())
+
+            var studies = new Studies();
+            if (data.Read())
             {
-                enrollment = Convert.ToInt32(data[0]);
-            }
-            if (enrollment != 1)
+                studies.IdStudy = Convert.ToInt32(data["IdStudy"]);
+                studies.Name = data["Name"].ToString();
+            } else
             {
                 throw new InvalidArgumentException();
             }
+            command.Parameters.Clear();
+
+
+            //Najświeższy semestr wybranego kierunku
+            command.CommandText = "select top 1 * from Enrollment where IdStudy = @studyId order by StartDate desc";
+            command.Parameters.AddWithValue("studyId", studies.IdStudy);
+            connection.Open();
+            data = command.ExecuteReader();
+
+            var enrollment = new Enrollment();
+            var enrollmentId = 0;
+            if (data.Read())
+            {
+                enrollment.IdEnrollment = Convert.ToInt32(data["IdEnrollment"]);
+                enrollmentId = enrollment.IdEnrollment;
+                enrollment.IdStudy = Convert.ToInt32(data["IdStudy"]);
+                enrollment.Semester = Convert.ToInt32(data["Semester"]);
+                enrollment.StartDate = Convert.ToDateTime(data["StartDate"]);
+            }
+            else
+            //dodanie jeśli nie istnieje 
+            {
+                command.CommandText = "select top 1 * from Enrollment order by IdEnrollment desc";
+                connection.Open();
+
+                data.Read();
+                enrollmentId = Convert.ToInt32(data["IdEnrollment"]) + 1;
+
+                command.CommandText = "insert into Enrollment values (@id, 1, @studyId, GETDATE())";
+                command.Parameters.AddWithValue("studyId", studies.IdStudy);
+                command.Parameters.AddWithValue("id", enrollmentId);
+                connection.Open();
+            }
+            command.Parameters.Clear();
+
+            //Czy prawidłowy index
+            command.CommandText = "select * from Student where IndexNumber =  @index";
+            command.Parameters.AddWithValue("index", enrollments.IndexNumber);
+            connection.Open();
+            data = command.ExecuteReader();
+            if (data.Read())
+            {
+                throw new InvalidArgumentException();
+            }
+            command.Parameters.Clear();
+
+            //Dodajemy studenta
+            command.CommandText = "insert into Student values (@index, @firstName, @lastName, @birthDate, @enrollment)";
+            command.Parameters.AddWithValue("index", enrollments.IndexNumber);
+            command.Parameters.AddWithValue("firstName", enrollments.FirstName);
+            command.Parameters.AddWithValue("lastName", enrollments.LastName);
+            command.Parameters.AddWithValue("birthDate", enrollments.BirthDate);
+            command.Parameters.AddWithValue("enrollment", enrollmentId);
+            connection.Open();
+            data = command.ExecuteReader();
+            command.Parameters.Clear();
+
+
+
 
 
             tran.Commit();
